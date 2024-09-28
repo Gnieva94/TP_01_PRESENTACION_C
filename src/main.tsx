13833d7dba5a248a46ee1c8862215cd015560b4a
Gnieva94 <gus_94@hotmail.com.ar>
2024-09-28 04:53:03 -0300

    add errorpage, modify main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,31 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { StrictMode } from 'react'
+import ErrorPage from './ErrorPage.tsx';
+import { Layout, Home, Projects} from './routes';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
   ,
 )
