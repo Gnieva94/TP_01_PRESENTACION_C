d4cafd6bda48a7c7a9d1664d09c77e304d22a773
Gnieva94 <gus_94@hotmail.com.ar>
2024-09-28 01:28:31 -0300

    inicio proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import soyProgramador from './assets/soyprogramador.gif'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Nieva Gustavo</h1>
+      <img src={soyProgramador} className="logo" alt="Soy Programador" />
+    </>
+  )
+}
+
+export default App
